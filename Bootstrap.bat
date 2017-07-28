fe1a743bea911a5c71d4c706c5233e786f816490
David Koloski <djkoloski@gmail.com>
2017-07-27 22:45:49 -0400

    Syntax error, spelled exist wrong

--- Bootstrap.bat
+++ Bootstrap.bat
@@ -1,3 +1,3 @@
-if not exists "Build" mkdir "Build"
+if not exist "Build" mkdir "Build"
 External\ninja\ninja.exe -C Build -f ..\Bootstrap.ninja Makey
 Bin\Win_x64_Release\Makey\Makey.exe platform=Win_x64 config=Release compiler=MSVC
