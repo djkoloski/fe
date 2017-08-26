744425e4afa4f905430b703899bd7298dc22d4e2
David Koloski <djkoloski@gmail.com>
2017-08-26 14:16:04 -0400

    Fix bootstrap batch file

--- Bootstrap.bat
+++ Bootstrap.bat
@@ -1,5 +1,5 @@
 @echo off
 git checkout -- Build\Win_x64_Release\Bootstrap.ninja Build\Win_x64_Release\FeGen\Bootstrap.ninja Build\Win_x64_Release\Fe\Bootstrap.ninja Build\Win_x64_Release\FeMake\Bootstrap.ninja
 External\ninja\ninja.exe -C Build\Win_x64_Release -f Bootstrap.ninja FeMake_codegen
-External\ninja\ninja.exe -C Build\Win_x64_Release -f Bootstrap.ninja FeMake
+External\ninja\ninja.exe -C Build\Win_x64_Release -f Bootstrap.ninja FeMake_test
 Bin\Win_x64_Release\FeMake\FeMake.exe platform=Win_x64 config=Release compiler=MSVC
